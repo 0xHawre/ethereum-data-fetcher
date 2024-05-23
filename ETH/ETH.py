21140789e051ae2868480faa68334388458dd0d7
Hawre <131952165+Hafxhak@users.noreply.github.com>
2024-05-24 03:11:33 +0330

    Update ETH.py

--- ETH/ETH.py
+++ ETH/ETH.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import random
 
 RPC_URLS = [
-    "https://eth1.lava.build/lava-referer-fbd614bb-40a2-46c0-9f86-675e78683794/", #replace with youre RPC 
+    "https://eth1.lava.build/lava-referer-fbd614bb-40a2-46c0-9f86-675e78683794/",
 
 ]
 
@@ -36,6 +36,10 @@ def main():
                 print(f"Ethereum Data from {url}: {json.dumps(eth_data, indent=4)}")
             else:
                 print(f"Failed to fetch Ethereum data from {url}.")
-            
+
             # Sleep for a random interval between 10 to 25 seconds
-            sleep_interval
+            sleep_interval = random.uniform(10, 25)
+            time.sleep(sleep_interval)
+
+if __name__ == "__main__":
+    main()
