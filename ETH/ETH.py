4c92ef46c89e103eafaee4b2bea6dd0469d64368
hawre <1384hakbari@gmail.com>
2024-05-24 02:26:22 +0330

    Initial commit: Add Ethereum data fetcher script and README

---
+++ ETH/ETH.py
@@ -0,0 +1,41 @@
+import requests
+import json
+import time
+from datetime import datetime
+import random
+
+RPC_URLS = [
+    "https://eth1.lava.build/lava-referer-fbd614bb-40a2-46c0-9f86-675e78683794/", #replace with youre RPC 
+
+]
+
+ETH_METHOD = {
+    "jsonrpc": "2.0",
+    "method": "eth_gasPrice",
+    "id": 1
+}
+
+def fetch_ethereum_data(url):
+    try:
+        response = requests.post(url, headers={"Content-Type": "application/json"}, data=json.dumps(ETH_METHOD))
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to fetch Ethereum data from {url}: {e}")
+        return None
+
+def main():
+    usage_count = 0
+    while True:
+        for url in RPC_URLS:
+            eth_data = fetch_ethereum_data(url)
+            if eth_data:
+                usage_count += 1
+                timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                print(f"[{timestamp}] RPC Call Count: {usage_count}")
+                print(f"Ethereum Data from {url}: {json.dumps(eth_data, indent=4)}")
+            else:
+                print(f"Failed to fetch Ethereum data from {url}.")
+            
+            # Sleep for a random interval between 10 to 25 seconds
+            sleep_interval
